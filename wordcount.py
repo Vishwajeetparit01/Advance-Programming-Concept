510797aad1a528de5933b0295d83e6ea0291def5
Vishwajeetparit01 <vishwajeetparit@gmail.com>
2026-09-03 10:40:48 +0530

    Add files via upload

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,32 @@
-sentence = input("Enter a sentence: ")
+# 
+import string
 
-words = sentence.split()
+with open("file.txt", "w") as file:
+    file.write("Python is easy. Python is powerful. Python is used for programming.")
 
-print("Total number of words =", len(words))
+filename = "file.txt"
+
+with open(filename, "r") as file:
+    text = file.read()
+
+text = text.lower()
+
+for char in string.punctuation:
+    text = text.replace(char, "")
+
+words = text.split()
+
+word_count = len(words)
+
+frequency = {}
+
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+
+sorted_words = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+
+print("Total number of words:", word_count)
+print("\nTop 10 most frequent words:")
+
+for word, count in sorted_words[:10]:
+    print(word, ":", count)
